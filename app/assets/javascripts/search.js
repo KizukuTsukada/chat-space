3f8cc3d6abe6e5bb023a8e9301985afe51b617ca
kizuku <tukada2947@yahoo.co.jp>
2019-07-04 20:34:33 +0900

    グループ編集機能

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,6 @@
 $(function () {
   var search_list = $('#user-search-result');
-  // ユーザーリスト作成HTML
+  
   function appendUser(user) {
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>${user.name}</p>
@@ -8,15 +8,12 @@ $(function () {
                 </div>`
     search_list.append(html);
   }
-  // ユーザー名が存在しない
   function appendNoUser(user) {
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>${user}</p>
                 </div>`
     search_list.append(html);
   }
-  // ユーザー検索
-  // イベント発火、keyupメソッド
   $('#user-search-field').on('keyup', function (e) {
     var input = $('#user-search-field').val();
     if (input !== "") {
@@ -28,11 +25,8 @@ $(function () {
           },
           dataType: 'json'
         })
-        // ユーザーの検索に成功
-        // 非同期通信の結果をdoneの関数の引数から受け取り、viewに追加するHTMLを作成
         .done(function (users) {
           $('#user-search-result').empty();
-          // jbuilderから送られてきた配列の情報によって場合分け、関数呼び出し
           if (users.length !== 0) {
             users.forEach(function (user) {
               appendUser(user);
@@ -41,13 +35,11 @@ $(function () {
             appendNoUser('一致るするユーザーはありません');
           }
         })
-        // ユーザーの検索に失敗
         .fail(function () {
           alert('ユーザーの検索に失敗しました');
         })
     }
   });
-  // 追加ユーザーリスト作成
   function addUser(user_id, user_name) {
     var html = `<div class='chat-group-user clearfix' id='chat-group-user-8'>
                   <input name='group[user_ids][]' type='hidden' value='${user_id}'>
@@ -56,14 +48,12 @@ $(function () {
                 </div>`
     $('#chat-group-users').append(html);
   }
-  // 追加ボタンクリック時の処理
   $('#user-search-result').on('click', '.chat-group-user__btn--add', function () {
     var user_id = $(this).data('user-id');
     var user_name = $(this).data('user-name');
     addUser(user_id, user_name);
     $(this).parent().remove();
   });
-  // 削除ボタンクリック時の処理
   $('#chat-group-users').on('click', '.chat-group-user__btn--remove', function () {
     $(this).parent().remove();
   });
