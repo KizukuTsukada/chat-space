c60cf1aa9ebeda54b291e8302431078491d7513c
kizuku <tukada2947@yahoo.co.jp>
2019-07-04 22:48:02 +0900

    Update search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,6 @@
 $(function () {
   var search_list = $('#user-search-result');
-  
+
   function appendUser(user) {
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>${user.name}</p>
