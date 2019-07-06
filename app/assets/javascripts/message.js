9bdba3691cc787fc71a0c803300e0fb2416dff72
kizuku <tukada2947@yahoo.co.jp>
2019-07-06 12:11:56 +0900

    不要なconsole.logを削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -57,7 +57,6 @@ $(function(){
 
 
     .done(function(messages) {
-      console.log("test");
       //追加するHTMLの入れ物を作る
       var insertHTML = '';
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
